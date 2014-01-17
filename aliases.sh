1e32a29d58f44657363088f635d51a1867a2fc8d
ahmadshah <ahmadshahhafizan@gmail.com>
2014-01-18 02:05:56 +0800

    aliases

---
+++ aliases.sh
@@ -0,0 +1,24 @@
+# git commands
+alias g-status="git status"
+alias g-add="git add"
+alias g-commit="git commit --signoff"
+alias g-pull="git pull"
+alias g-remote-add="git remote add"
+alias g-remote-del="git remote remove"
+
+# laravel commands
+alias get-me-laravel="composer create-project laravel/laravel"
+alias get-me-orchestra="composer create-project orchestra/platform"
+alias artisan="php artisan"
+
+# composer commands
+alias get-this-composer="composer install --dev --prefer-dist"
+alias update-this-composer="composer update --dev --prefer-dist"
+
+# Testing
+alias run-unit-test="phpunit"
+
+# others
+alias sudoers="sudo"
+alias restart-apache="sudo apachectl -k restart"
+alias get-ssh-key="cat ~/.ssh/id_rsa.pub | pbcopy && echo 'SSH key copied'"
